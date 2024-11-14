25c7af842c347da8dadfd56cf52f669930ec9aff
fatmapostaci <fatmapostaci@gmail.com>
2024-11-14 18:45:50 +0300

    fatmacommitted18.45

--- src/team/fatma.java
+++ src/team/fatma.java
@@ -3,5 +3,6 @@ package team;
 public class fatma {
     public static void main(String[] args) {
         System.out.println("fatma class açtı 18.28");
+        System.out.println("fatma classta değişiklik yapıyor 18.44");
     }
 }
